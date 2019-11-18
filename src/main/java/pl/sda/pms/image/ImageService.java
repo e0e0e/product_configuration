e77f5dcb84b2580c132206df3f5484cd360b243c
grzes <szkop.grzegorz@gmail.com>
2019-11-18 22:28:43 +0100

    login correct

--- src/main/java/pl/sda/pms/image/ImageService.java
+++ src/main/java/pl/sda/pms/image/ImageService.java
@@ -6,7 +6,9 @@ import java.awt.datatransfer.Transferable;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
@@ -15,13 +17,18 @@ import java.util.Map;
 import javax.imageio.ImageIO;
 import javax.xml.bind.DatatypeConverter;
 
+import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
 import org.springframework.stereotype.Service;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
 @Service
 public class ImageService {
+	private static Config config = ConfigFactory.load();
 
 	public void importImage() throws Exception {
 		System.err.println("usage: java clipimg [filename]");
@@ -79,88 +86,109 @@ public class ImageService {
 		return res;
 
 	}
-	
+
 	public void readFtp() {
-		 String server = "wielon.c0.pl";
-	        int port = 21;
-	        String user = "wielon";
-	        String pass = "krowa111";
-	 
-	        FTPClient ftpClient = new FTPClient();
-	 
-	        try {
-	 
-	            ftpClient.connect(server, port);
-	            showServerReply(ftpClient);
-	            System.out.println("rep1");
-	            int replyCode = ftpClient.getReplyCode();
-	            if (!FTPReply.isPositiveCompletion(replyCode)) {
-	                System.out.println("Connect failed");
-	                return;
-	            }
-	 
-	            boolean success = ftpClient.login(user, pass);
-	            showServerReply(ftpClient);
-	            System.out.println("rep2");
-	            if (!success) {
-	                System.out.println("Could not login to the server");
-	                return;
-	            }
-	 
-	            // Lists files and directories
-	            FTPFile[] files1 = ftpClient.listFiles("/public_ftp");
-	            printFileDetails(files1);
-	 
-	            // uses simpler methods
-	            String[] files2 = ftpClient.listNames();
-	            printNames(files2);
-	 
-	 
-	        } catch (IOException ex) {
-	            System.out.println("Oops! Something wrong happened");
-	            ex.printStackTrace();
-	        } finally {
-	            // logs out and disconnects from server
-	            try {
-	                if (ftpClient.isConnected()) {
-	                    ftpClient.logout();
-	                    ftpClient.disconnect();
-	                }
-	            } catch (IOException ex) {
-	                ex.printStackTrace();
-	            }
-	        }
+		String server = config.getString("ftpServer");
+		int port = config.getInt("ftpPort");
+		String user = config.getString("ftpUser");
+		String pass = config.getString("ftpPass");
+
+//		String server = "ftp.wielton.home.pl";
+//		int port = 21;
+//		String user = "gszkop";
+//		String pass = "S!R!cpTfCdM8";
+
+		FTPClient ftpClient = new FTPClient();
+
+		try {
+
+			ftpClient.connect(server, port);
+			showServerReply(ftpClient);
+
+			int replyCode = ftpClient.getReplyCode();
+			if (!FTPReply.isPositiveCompletion(replyCode)) {
+				System.out.println("Connect failed");
+				return;
+			}
+
+			ftpClient.enterLocalPassiveMode();
+			boolean success = ftpClient.login(user, pass);
+
+			if (!success) {
+				System.out.println("Could not login to the server");
+				return;
+			} else {
+				System.out.print("Loggin succesfull");
+			}
+
+			// Lists files and directories
+			FTPFile[] files1 = ftpClient.listFiles("/wielon.c0.pl/img");
+			printFileDetails(files1);
+
+			// upload
+//	            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+//	            
+//	            File firstLocalFile = new File("/home/grzes/Documents/pliki/canvasImage.png");
+//	            
+//	            String firstRemoteFile = "/wielon.c0.pl/img/canvas.png";
+//	            InputStream inputStream = new FileInputStream(firstLocalFile);
+//	            System.out.println("Start uploading first file");
+//	            boolean done = ftpClient.storeFile(firstRemoteFile, inputStream);
+//	            inputStream.close();
+
+//	            if (done) {
+//	                System.out.println("The first file is uploaded successfully.");
+//	            }
+
+			// uses simpler methods
+			String[] files2 = ftpClient.listNames();
+			printNames(files2);
+
+		} catch (IOException ex) {
+			System.out.println("Oops! Something wrong happened");
+			ex.printStackTrace();
+		} finally {
+			// logs out and disconnects from server
+			try {
+				if (ftpClient.isConnected()) {
+					ftpClient.logout();
+					ftpClient.disconnect();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
 	}
-	
-    private static void printFileDetails(FTPFile[] files) {
-        DateFormat dateFormater = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        for (FTPFile file : files) {
-            String details = file.getName();
-            if (file.isDirectory()) {
-                details = "[" + details + "]";
-            }
-            details += "\t\t" + file.getSize();
-            details += "\t\t" + dateFormater.format(file.getTimestamp().getTime());
- 
-            System.out.println(details);
-        }
-    }
- 
-    private static void printNames(String files[]) {
-        if (files != null && files.length > 0) {
-            for (String aFile: files) {
-                System.out.println(aFile);
-            }
-        }
-    }
- 
-    private static void showServerReply(FTPClient ftpClient) {
-        String[] replies = ftpClient.getReplyStrings();
-        if (replies != null && replies.length > 0) {
-            for (String aReply : replies) {
-                System.out.println("SERVER: " + aReply);
-            }
-        }
-    }
+
+	private static void printFileDetails(FTPFile[] files) {
+		DateFormat dateFormater = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		for (FTPFile file : files) {
+			String details = file.getName();
+			if (file.isDirectory()) {
+				details = "[" + details + "]";
+			}
+			details += "\t\t" + file.getSize();
+			details += "\t\t" + dateFormater.format(file.getTimestamp().getTime());
+
+			System.out.println(details);
+		}
+	}
+
+	private static void printNames(String files[]) {
+		if (files != null && files.length > 0) {
+			for (String aFile : files) {
+				System.out.println(aFile);
+			}
+		}
+	}
+
+	private static void showServerReply(FTPClient ftpClient) {
+		String[] replies = ftpClient.getReplyStrings();
+		if (replies != null && replies.length > 0) {
+			for (String aReply : replies) {
+				System.out.println("SERVER: " + aReply);
+			}
+		}
+	}
 
 }
